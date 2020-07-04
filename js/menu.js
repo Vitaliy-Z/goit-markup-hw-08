946d82707f2b7f6cb95d85c4c3b82ae3fad6b1fe
Vitaliy Zvieriev <zverew91@gmail.com>
2020-07-04 16:26:47 +0300

    Добавляет адаптивность кроме изображений

---
+++ js/menu.js
@@ -0,0 +1,51 @@
+// Скрипт для открытия меню
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
+
+// Скрипт для отправки формы
+
+(() => {
+  document.querySelector(".js-speaker-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
+
+    e.currentTarget.reset();
+  });
+})();
+
+// Скрипт для модалки
+
+const refs = {
+  openModalBtn: document.querySelector("[data-open-modal]"),
+  closeModalBtn: document.querySelector("[data-close-modal]"),
+  backdrop: document.querySelector("[data-backdrop]"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+refs.backdrop.addEventListener("click", logBackdropClick);
+
+function toggleModal() {
+  refs.backdrop.classList.toggle("is-hidden");
+}
+
+function logBackdropClick() {
+  console.log("Это клик в бекдроп");
+}
